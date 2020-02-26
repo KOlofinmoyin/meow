b0583182c3aa0bf495f81fc7504005c8751b2912
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-02-26 22:54:37 +0000

    spiked 'search' endpoint functionality.

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
+require 'net/http'
 
 class Meow < Sinatra::Base
 
+  get '/' do
+    endpoint = 'https://api.thecatapi.com/v1/images/search?breed_ids=beng&limit=3&page=100&order=DESC'
+    puts 'Working'
+    uri = URI.parse(URI.encode(endpoint))
+    api_response = Net::HTTP.get(uri)
+    @data = JSON.parse(api_response)
+    erb :index
+  end
+
   run! if app_file == $0
+
 end
